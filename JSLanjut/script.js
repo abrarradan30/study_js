f93329cf3cc69b3b0d66afbc20f5f72586697cbe
abrarradan30 <abradan84@gmail.com>
2023-08-03 19:16:35 +0700

    function declaration

--- JSLanjut/script.js
+++ JSLanjut/script.js
@@ -1,6 +1,7 @@
 // Cara untuk membuat object pada javascript
 // 1. Object Literal 
-/*
+/* 
+// PROBLEM : tidak efektif untuk obyek yg banyak
 let mahasiswa1 = {
     nama: 'Radan',
     energi: 10,
@@ -20,31 +21,39 @@ let mahasiswa2 = {
 } */
 
 // 2. Function Declaration
-/*
-function Mahasiswa(nama, energi) {
-    let mahasiswa = {};
-    mahasiswa.nama =  nama;
-    mahasiswa.energi = energi;
 
-    mahasiswa.makan = function(porsi) {
+const methodMahasiswa = {
+    makan: function(porsi) {
         this.energi += porsi;
         console.log(`Halo ${this.nama}, selamat makan!`);
-    }
+    },
 
-    mahasiswa.main = function(jam) {
+    main: function(jam) {
         this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat tidur!`);
+    },
+
+    tidur: function(jam) {
+        this.energi += jam * 2;
         console.log(`Halo ${this.nama}, selamat bermain!`);
     }
-
+};
+ 
+function Mahasiswa(nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama =  nama;
+    mahasiswa.energi = energi;
+    
     return mahasiswa;
 }
 
 let radan = Mahasiswa('Radan', 10);
 let dody = Mahasiswa ('Dody', 20);
-*/
+
 
 // 3. Constructor Function
 // keywoard new
+/*
 function Mahasiswa(nama, energi) {
     this.nama =  nama;
     this.energi = energi;
@@ -60,5 +69,6 @@ function Mahasiswa(nama, energi) {
     }
 }
 let radan = new Mahasiswa ('Radan', 10);
+*/
 
 // 4. Object create
