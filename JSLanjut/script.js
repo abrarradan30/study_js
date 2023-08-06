2969003b1196e1c6fb214c15803922285c7f07f0
abrarradan30 <abradan84@gmail.com>
2023-08-06 09:57:59 +0700

    modified prototype

--- JSLanjut/script.js
+++ JSLanjut/script.js
@@ -21,7 +21,7 @@ let mahasiswa2 = {
 } */
 
 // 2. Function Declaration
-
+/*
 const methodMahasiswa = {
     makan: function(porsi) {
         this.energi += porsi;
@@ -49,6 +49,64 @@ function Mahasiswa(nama, energi) {
 
 let radan = Mahasiswa('Radan', 10);
 let dody = Mahasiswa ('Dody', 20);
+*/
+
+/*
+function Mahasiswa(nama, energi) {
+    this.nama =  nama;
+    this.energi = energi;
+}
+
+Mahasiswa.prototype.makan = function(porsi) {
+    this.energi += porsi;
+    return `Halo ${this.nama}, selamat makan!`;
+}
+
+Mahasiswa.prototype.main = function(jam) {
+    this.energi -= jam;
+    return `Halo ${this.nama}, selamat bermain!`;
+}
+
+Mahasiswa.prototype.tidur = function(jam) {
+    this.energi += jam * 2;
+    return `Halo ${this.nama}, selamat tidur!`;
+}
+
+let radan = new Mahasiswa('Radan', 10);
+*/
+
+// versi class
+/*
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan (porsi) {
+        this.energi += porsi;
+        return `Halo ${this.nama}, selamat makan!`;
+    }
+    
+    main (jam) {
+        this.energi -= jam;
+        return `Halo ${this.nama}, selamat bermain!`;
+    }
+    
+    tidur (jam) {
+        this.energi += jam * 2;
+        return `Halo ${this.nama}, selamat tidur!`;
+    }
+    
+}
+
+let radan = new Mahasiswa('Radan', 10);
+let dody = new Mahasiswa('Dody', 20);
+*/
+
+let angka = 1;
+//  console.log(angka);
+
 
 
 // 3. Constructor Function
