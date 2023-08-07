9de0104ce1eb4c7d59649b98a2413a921511a8dd
abrarradan30 <abradan84@gmail.com>
2023-08-07 21:28:44 +0700

    modified execution

--- JSLanjut/execution.js
+++ JSLanjut/execution.js
@@ -1,3 +1,13 @@
 // 2.1 EXECUTION CONTEXT, HOISTING & SCOPE
 
+var nama = 'Radan';
 console.log(nama);
+
+/* creation phase pada Global Context
+nama var = undefined
+nama function = fn()
+hoisting
+window = global object 
+this = window
+execution phase
+*/
