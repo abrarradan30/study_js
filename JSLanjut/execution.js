f4f157fa886c376955a922bf2d3c057b9fa45077
abrarradan30 <abradan84@gmail.com>
2023-08-10 20:16:33 +0700

    modified execution

--- JSLanjut/execution.js
+++ JSLanjut/execution.js
@@ -1,5 +1,5 @@
 // 2.1 EXECUTION CONTEXT, HOISTING & SCOPE
-
+/*
 var nama = 'Radan';
 console.log(nama);
 
@@ -10,4 +10,33 @@ hoisting
 window = global object 
 this = window
 execution phase
+*/
+
+/*
+var nama = 'Radan';
+var umur = 22;
+
+console.log(sayHello());
+
+function sayHello() {
+   return `Halo, namas saya ${nama}, saya ${umur} tahun.`; 
+}
+*/
+/* function membuat Local Execution Context
+yang didalamnya terdapat creation dan execution phase
+window
+arguments
+hoisting
+*/
+
+/*
+var nama = 'Radan';
+var username = '@abrarradan';
+
+function cetakURL(username) {
+    var instagramURL = 'http://instagram.com/';
+    return instagramURL + username;
+}
+
+console.log(cetakURL(username));
 */
