051e40b3017dc4a64b93830d60e4a8d11369342d
abrarradan30 <abradan84@gmail.com>
2023-10-05 13:10:25 +0700

    add closure

--- JSLanjut/closure.js
+++ JSLanjut/closure.js
@@ -1,3 +1,5 @@
+// closure : konsep di mana sebuah fungsi dapat mengakses variabel-variabel yang berada di luar cakupan (scope) fungsi tersebut, bahkan setelah fungsi tersebut selesai dijalankan. 
+
 /* function init() {
     // let nama = "Abrar";
     return function(nama) {
@@ -24,3 +26,14 @@ let selamatMalam = ucapkanSalam('Malam');
 
 console.dir(selamatMalam('Radan'));
 */
+
+let add = (function() {
+    let counter = 0;
+    return function() {
+        return ++counter;
+    }
+})();
+
+console.log(add());
+console.log(add());
+console.log(add());
