b9f40fa934cb45d530bbbb482b3f41c17eac2aef
abrarradan30 <abradan84@gmail.com>
2023-10-05 12:18:52 +0700

    add use factory function in closure

--- JSLanjut/closure.js
+++ JSLanjut/closure.js
@@ -1,4 +1,4 @@
-function init() {
+/* function init() {
     // let nama = "Abrar";
     return function(nama) {
         console.log(nama);
@@ -8,3 +8,19 @@ function init() {
 let panggilNama = init();
 panggilNama('Abrar');
 panggilNama('Radan');
+*/
+
+/*
+function ucapkanSalam(waktu) {
+    return function(nama) {
+        console.log(`Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan!`);
+    }
+}
+
+// factory function
+let selamatPagi = ucapkanSalam('Pagi');
+let selamatSiang = ucapkanSalam('Siang');
+let selamatMalam = ucapkanSalam('Malam');
+
+console.dir(selamatMalam('Radan'));
+*/
