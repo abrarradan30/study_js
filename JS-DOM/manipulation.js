e2e3778b2fdbadaa318d77148bc95d1f025b8f59
abrarradan30 <abradan84@gmail.com>
2024-09-06 20:19:16 +0700

    add DOM Events

--- JS-DOM/manipulation.js
+++ JS-DOM/manipulation.js
@@ -19,37 +19,37 @@
 // MANIPULASI DOM
 
 // buat elemen baru
-const pBaru = document.createElement('p');
-const teksPBaru = document.createTextNode('Paragraf Baru');
+// const pBaru = document.createElement('p');
+// const teksPBaru = document.createTextNode('Paragraf Baru');
 // simpan tulisan pada paragraf 
-pBaru.appendChild(teksPBaru);
+// pBaru.appendChild(teksPBaru);
 // simpan pBaru diakhir section A
-const sectionA = document.getElementById('a');
-sectionA.appendChild(pBaru);
+// const sectionA = document.getElementById('a');
+// sectionA.appendChild(pBaru);
 
-const liBaru = document.createElement('li');
-const teksLiBaru = document.createTextNode('Item Baru');
-liBaru.appendChild(teksLiBaru);
+// const liBaru = document.createElement('li');
+// const teksLiBaru = document.createTextNode('Item Baru');
+// liBaru.appendChild(teksLiBaru);
 
-const ul = document.querySelector('section#b ul');
-const li2 = ul.querySelector('li:nth-child(2)');
+// const ul = document.querySelector('section#b ul');
+// const li2 = ul.querySelector('li:nth-child(2)');
 
-ul.insertBefore(liBaru, li2);
+// ul.insertBefore(liBaru, li2);
 
 // remove
-const link = document.getElementsByTagName('a')[0];
-sectionA.removeChild(link);
+// const link = document.getElementsByTagName('a')[0];
+// sectionA.removeChild(link);
 
 // replace
-const sectionB = document.getElementById('b');
-const p4 = sectionB.querySelector('p');
-const h2Baru = document.createElement('h2');
-const teksH2Baru = document.createTextNode('Judul baru!');
+// const sectionB = document.getElementById('b');
+// const p4 = sectionB.querySelector('p');
+// const h2Baru = document.createElement('h2');
+// const teksH2Baru = document.createTextNode('Judul baru!');
 
-h2Baru.appendChild(teksH2Baru);
-sectionB.replaceChild(h2Baru, p4);
+// h2Baru.appendChild(teksH2Baru);
+// sectionB.replaceChild(h2Baru, p4);
 
 // update
-pBaru.style.backgroundColor = 'lightgreen';
-liBaru.style.backgroundColor = 'lightgreen';
-h2Baru.style.backgroundColor = 'lightgreen';
+// pBaru.style.backgroundColor = 'lightgreen';
+// liBaru.style.backgroundColor = 'lightgreen';
+// h2Baru.style.backgroundColor = 'lightgreen';
